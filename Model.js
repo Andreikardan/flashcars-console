898cb6d35e53e34e685cf09660f5c18e05cab075
Nikita <felasso3212@gmail.com>
2024-09-13 10:14:57 +0300

    Fixed-model and Test

--- Model.js
+++ Model.js
@@ -1,6 +1,9 @@
-const inquirer = require("inquirer");
-const fs = require("fs").promises;
-const Test = require("./Test.js");
+const inquirer = require('inquirer');
+const fs = require('fs').promises;
+const Test = require('./Test.js');
+
+
+
 
 class ChoiceTestFile {
   static choiceFile() {
@@ -9,20 +12,19 @@ class ChoiceTestFile {
         inquirer
           .prompt([
             {
-              type: "list",
-              name: "Choice",
-              message: "Выбери тему викторины:",
+              type: 'list',
+              name: 'Choice',
+              message: 'Выбери тему викторины:',
               choices: [
-                { name: "Общие Знания" },
-                { name: "Правила дорожного движения" },
-                { name: "Путешествие на Луну" },
+                { name: 'Общие Знания' },
+                { name: 'Правила дорожного движения' },
+                { name: 'Мультфильмы' },
               ],
             },
           ])
-          .then((answers) => Test.writeFile(answers.Choice))
-
-      } catch ({error}) {
-        console.error("Ошибка при чтении файлов:", error);
+          .then((answers) => Test.writeFile(answers.Choice));
+      } catch ({ error }) {
+        console.error('Ошибка при чтении файлов:', error);
       }
     })();
   }
--- Test.js
+++ Test.js
@@ -1,19 +1,24 @@
 const inquirer = require('inquirer');
 const fs = require('fs').promises;
 
-
-
 class Test {
   static writeFile(name) {
-
-    async () => {
+    (async () => {
       try {
-        console.log(name);
-
-      } catch ({error}) {
+        if (name === 'Общие Знания') {
+          const newPath = await fs.readFile('./topics/general-knowledge.txt', 'utf-8');
+          return newPath;
+        } if (name === 'Правила дорожного движения') {
+          const newPath = await fs.readFile('./topics/traffic-laws.txt', 'utf-8');
+          return newPath;
+        } if (name === 'Мультфильмы') {
+          const newPath = await fs.readFile('./topics/cartoons-and-movies.txt', 'utf-8');
+          return newPath;
+        }
+      } catch ({ error }) {
         console.error('Ошибка при чтении файлов:', error);
       }
-    };
+    })();
   }
 }
 
