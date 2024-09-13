d57733e60399eb08f399811b3f76757f56bf167a
Nikita <felasso3212@gmail.com>
2024-09-13 11:02:03 +0300

    Fixed classes test and modul

--- Model.js
+++ Model.js
@@ -2,34 +2,29 @@ const inquirer = require('inquirer');
 const fs = require('fs').promises;
 const Test = require('./Test.js');
 
-
-
-
 class ChoiceTestFile {
-  static choiceFile() {
-    (async () => {
-      try {
-        inquirer
-          .prompt([
-            {
-              type: 'list',
-              name: 'Choice',
-              message: 'Выбери тему викторины:',
-              choices: [
-                { name: 'Общие Знания' },
-                { name: 'Правила дорожного движения' },
-                { name: 'Мультфильмы' },
-              ],
-            },
-          ])
-          .then((answers) => Test.writeFile(answers.Choice));
-      } catch ({ error }) {
-        console.error('Ошибка при чтении файлов:', error);
-      }
-    })();
+  static async chooseTopic() {
+    try {
+      const result = await inquirer
+        .prompt([
+          {
+            type: 'list',
+            name: 'Choice',
+            message: 'Выбери тему викторины:',
+            choices: [
+              { name: 'Общие Знания' },
+              { name: 'Правила дорожного движения' },
+              { name: 'Мультфильмы' },
+            ],
+          },
+        ]);
+      return result;
+    } catch ({ error }) {
+      console.error('Ошибка при чтении файлов:', error);
+    }
   }
 }
 
-ChoiceTestFile.choiceFile();
+ChoiceTestFile.chooseTopic();
 
 module.exports = ChoiceTestFile;
